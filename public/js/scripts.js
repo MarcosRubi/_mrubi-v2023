d96c8c1598c1d284a5808a44806015ae95113c9d
MarcosRubi <danielhernandez9980@gmail.com>
2022-11-24 11:41:45 -0600

    Disenno y contenido del menu

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,7 +2,6 @@ window.onload = ()=>{
     const btnMenuHamburguer = document.querySelector('.menu__hamburguer')
     const btnMenuClose = document.querySelector('.menu__close')
     const menuContent = document.querySelector('.menu__content')
-    const logoMenuContent = document.querySelector('.menu__content .logo')
 
     btnMenuHamburguer.onclick = ()=>{
         menuContent.classList.add('visible')
@@ -10,7 +9,5 @@ window.onload = ()=>{
     btnMenuClose.onclick = ()=>{
         menuContent.classList.remove('visible')
     }
-    logoMenuContent.onclick = ()=>{
-        menuContent.classList.remove('visible')
-    }
+
 }
